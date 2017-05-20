5119b877b5c74fc12a2c9ace7ba52aeb90e9dd52
bkold <bakolden5@gmail.com>
2017-05-20 01:47:11 -0700

    Threaded

--- src/finder.adb
+++ src/finder.adb
@@ -1,10 +1,12 @@
 with Dirent; use Dirent;
 with Ada.Text_IO;
+with Ada.Strings;
+with Ada.Strings.Fixed;
 
 package body Finder is
 
 	procedure Find_Start (
-		Directory, Token : in String; 
+		Directory, Token : in String;
 		Desired_Depth : in Natural := Natural'Last)
 	is
 	begin
@@ -14,8 +16,8 @@ package body Finder is
 	end;
 
 	procedure Find (
-		Directory : in String; 
-		Depth : in Natural) 
+		Directory : in String;
+		Depth : in Natural)
 	is
 		Local_Search : Directory_Type;
 		D_Entry : Directory_Entry;
@@ -28,6 +30,7 @@ package body Finder is
 
 				declare
 					Entry_Name : constant String := Name(D_Entry);
+					Thread_Check_Return : Integer;
 				begin
 					if GNAT.Regexp.Match(Entry_Name, Match_Token) then
 						Write(Directory & '/' & Entry_Name);
@@ -36,7 +39,13 @@ package body Finder is
 					if Depth < Max_Depth then
 						if Mode(D_Entry) = DIR and
 						then Entry_Name /= ".." and then Entry_Name /= "." then
-							Find(Directory & '/' & Entry_Name, Depth+1);
+							Task_Pool_Status.Check(Thread_Check_Return);
+							case (Thread_Check_Return) is
+								when 1 => Thread_1.Run(Directory & '/' & Entry_Name, Depth+1);
+								when 2 => Thread_2.Run(Directory & '/' & Entry_Name, Depth+1);
+								when 3 => Thread_3.Run(Directory & '/' & Entry_Name, Depth+1);
+								when others => Find(Directory & '/' & Entry_Name, Depth+1);
+							end case;
 						end if;
 					end if;
 				end;
@@ -50,5 +59,41 @@ package body Finder is
 	begin
 		Ada.Text_IO.Put_Line(Directory);
 	end Write;
+
+	task body Thread is
+		use Ada.Strings;
+		use Ada.Strings.Fixed;
+		Directory : String(1 .. 200);
+		Local_Depth : Natural;
+	begin
+		loop
+			select
+				accept Run (D : in String; Depth : in Natural) do
+					Move(D, Directory);
+					Local_Depth := Depth;
+				end Run;
+				Find(Trim(Directory, Both), Local_Depth);
+				Task_Pool_Status.End_Thread(Num);
+			or
+				terminate;
+			end select;
+		end loop;
+	end Thread;
+
+	protected body Task_Pool_Status is
+		procedure Check (Value : out Integer) is
+		begin
+			if    Status(1) then Value := 1; Status(1) := False;
+			elsif Status(2) then Value := 2; Status(2) := False;
+			elsif Status(3) then Value := 3; Status(3) := False;
+			else Value := 0;
+			end if;
+		end Check;
+
+		procedure End_Thread (Thread_Num : in Integer) is
+		begin
+			Status(Thread_Num) := True;
+		end End_Thread;
+	end Task_Pool_Status;
 
 end Finder;
--- src/finder.ads
+++ src/finder.ads
@@ -6,21 +6,38 @@ package Finder is
 		Plain,
 		Regular_Expression);
 
-	procedure Find_Start 
-		(Directory, Token : in String; 
-		 Desired_Depth : in Natural := Natural'Last);
+	procedure Find_Start (
+		Directory, Token : in String;
+		Desired_Depth : in Natural := Natural'Last);
 
 private
 
-	
 	Max_Depth : Natural;
 
 	Match_Token : GNAT.Regexp.Regexp;
 
 	procedure Find (
-		Directory : in String; 
+		Directory : in String;
 		Depth : in Natural);
 
 	procedure Write (Directory : in String);
+
+	task type Thread (Num : Integer) is
+		entry Run (D : in String; Depth : in Natural);
+	end Thread;
+
+	type Thread_Access is access all Thread;
+	type Bool_Array is array (1 .. 3) of Boolean;
+
+	Thread_1 : Thread(1);
+	Thread_2 : Thread(2);
+	Thread_3 : Thread(3);
+
+	protected Task_Pool_Status is
+		procedure Check (Value : out Integer);
+		procedure End_Thread (Thread_Num : in Integer);
+	private
+		Status : Bool_Array := (others=>True);
+	end Task_Pool_Status;
 
 end Finder;
